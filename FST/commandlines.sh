820c378afa63d982a97b7d5c169017894d800821
Parul Johri <48296846+paruljohri@users.noreply.github.com>
2021-04-24 17:06:16 -0400

    Create commandlines.sh

---
+++ FST/commandlines.sh
@@ -0,0 +1,4 @@
+#To get FST for slinding windows:
+python statistics_slidingwindow_pylibseq_general_reps_fst.py -winSize 500 -stepSize 500 -regionLen 10000 -numIndvPop1 100 -numIndvPop2 100 -input_folder /path/to/${foldername} -output_folder /path/to/output/directory -output_prefix ${foldername}
+
+
